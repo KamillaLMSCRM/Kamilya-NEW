339154931aa068e52257607c4537718606aec751
askar0007amirkhanov <askar0007amirkhanov@gmail.com>
2026-06-21 11:04:03 +0500

    Fix: Simplify dependencies, disable Turbopack, fix module type

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,4 @@
-#!/usr/bin/env node
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
   reactStrictMode: true,
   images: {
@@ -9,22 +7,11 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'cdn.lms.kml.kz',
       },
-      {
-        protocol: 'http',
-        hostname: 'minio',
-        port: '9000',
-      },
     ],
   },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/login',
-        permanent: false,
-      },
-    ];
+  experimental: {
+    serverActions: true,
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
